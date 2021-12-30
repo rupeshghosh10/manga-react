d5ce9e960f0de4737383c1dc403af1109f6da7cc
Rupesh <rupeshghosh10@gmail.com>
2021-12-30 10:14:06 +0530

    navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,56 @@
 import { FaBars } from 'react-icons/fa';
 import styles from './Navbar.module.css';
 import logo from '../../Icon.svg';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const changeWidth = () => {
+    setScreenWidth(window.innerWidth);
+  }
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', changeWidth);
+  }, []);
+
   return (
     <nav>
       <div className={styles.header}>
-        <button className={styles.toggle}>
+        <button className={`${styles.toggle} ${toggle ? styles.toggleRotate : ''}`} onClick={handleToggle}>
           <FaBars />
         </button>
         <img src={logo} alt="logo" className={styles.logo} />
       </div>
-      <div className={styles.linksContainer}>
-        <ul className={styles.links}>
-          <li>
-            <a href="/">Discover</a>
-          </li>
-          <li>
-            <a href="/">Search</a>
-          </li>
-          <li>
-            <a href="/">History</a>
-          </li>
-          <li>
-            <a href="/">Saved</a>
-          </li>
-          <li>
-            <a href="/">Random</a>
-          </li>
-          <li>
-            <a href="/">Github</a>
-          </li>
-        </ul>
-      </div>
+      {(toggle || screenWidth > 625) &&
+        <div className={styles.linksContainer}>
+          <ul className={styles.links}>
+            <li>
+              <a href="/">Discover</a>
+            </li>
+            <li>
+              <a href="/">Search</a>
+            </li>
+            <li>
+              <a href="/">History</a>
+            </li>
+            <li>
+              <a href="/">Saved</a>
+            </li>
+            <li>
+              <a href="/">Random</a>
+            </li>
+            <li>
+              <a href="/">Github</a>
+            </li>
+          </ul>
+        </div>}
       <div className={styles.account}>
         <a href='/'>Log In</a>
       </div>
