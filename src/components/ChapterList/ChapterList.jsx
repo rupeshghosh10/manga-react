98e04094e8073954dec1d1e7feb5cd941eae8fb9
Rupesh <rupeshghosh10@gmail.com>
2022-01-09 16:39:37 +0530

    chapter list search and sort

--- src/components/ChapterList/ChapterList.jsx
+++ src/components/ChapterList/ChapterList.jsx
@@ -10,6 +10,8 @@ const ChapterList = ({ mangaId }) => {
   const [chapterList, setChapterList] = useState([]);
   const [chapterAvailable, setChapterAvailable] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortOrder, setSortOrder] = useState('desc');
+  const [searchChapter, setSearchChapter] = useState('');
 
   useEffect(() => {
     mangadexApi.getChapterList(mangaId, 0).then(res => {
@@ -18,17 +20,28 @@ const ChapterList = ({ mangaId }) => {
         setIsLoading(false);
       }
       else {
-        setChapterList(res.data);
         if (res.data.length < 100) {
+          setChapterList(res.data);
           setIsLoading(false);
         }
         else {
-          let result = [...res.data];
+          let result = res.data;
           fetchAllChapter(result, 100);
         }
       }
     });
   }, [mangaId]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      if (sortOrder === 'asc') {
+        setChapterList([...chapterList.sort((a, b) => a.attributes.chapter - b.attributes.chapter)]);
+      }
+      else if (sortOrder === 'desc') {
+        setChapterList([...chapterList.sort((b, a) => a.attributes.chapter - b.attributes.chapter)]);
+      }
+    }
+  }, [sortOrder]);
 
   const fetchAllChapter = (result, offset) => {
     mangadexApi.getChapterList(mangaId, offset).then(res => {
@@ -40,6 +53,20 @@ const ChapterList = ({ mangaId }) => {
       }
       fetchAllChapter(result, offset + 100);
     });
+  }
+
+  const handleSortOrderChange = e => {
+    setSortOrder(e.target.value);
+  }
+
+  const handleSearchChapterChange = e => {
+    setSearchChapter(e.target.value.trimStart());
+  }
+
+  const chapterFilter = chapter => {
+    const title = chapter.attributes.title.toLowerCase();
+    const chapterNo = chapter.attributes.chapter; 
+    return title.includes(searchChapter.toLowerCase().trim()) || chapterNo.includes(searchChapter.trim());
   }
 
   if (isLoading) {
@@ -59,19 +86,36 @@ const ChapterList = ({ mangaId }) => {
   }
 
   return (
-    <ul className={styles.chapterList}>
-      {chapterList.map((chapter, i) => (
-        <li className={styles.chapterDetail} key={i}>
-          <div className={styles.chapter}>
-            <p className={styles.chapterName}>Chapter {chapter.attributes.chapter} : {chapter.attributes.title}</p>
-            <p className={styles.chapterDate}>{moment(chapter.attributes.updatedAt).format('MMM DD, YYYY')}</p>
-          </div>
-          <div className={styles.scanlationGroup}>
-            {helper.findScanlationGroup(chapter) && <p>{helper.findScanlationGroup(chapter).attributes.name}</p>}
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className={styles.control}>
+        <div className={styles.inputContainer}>
+          <p>Search Chapter</p>
+          <input className={styles.input} value={searchChapter} onChange={handleSearchChapterChange} />
+        </div>
+        <div className={styles.sortContainer}>
+          <p>Sort By</p>
+          <select className={styles.select} value={sortOrder} onChange={handleSortOrderChange}>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+      </div>
+      <ul className={styles.chapterList}>
+        {chapterList
+          .filter(chapter => chapterFilter(chapter))
+          .map((chapter, i) => (
+            <li className={styles.chapterDetail} key={i}>
+              <div className={styles.chapter}>
+                <p className={styles.chapterName}>Chapter {chapter.attributes.chapter} : {chapter.attributes.title}</p>
+                <p className={styles.chapterDate}>{moment(chapter.attributes.updatedAt).format('MMM DD, YYYY')}</p>
+              </div>
+              <div className={styles.scanlationGroup}>
+                {helper.findScanlationGroup(chapter) && <p>{helper.findScanlationGroup(chapter).attributes.name}</p>}
+              </div>
+            </li>
+          ))}
+      </ul>
+    </>
   )
 }
 
